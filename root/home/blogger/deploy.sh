3a62b1d607d11df2c4362a9f6a04bb056a90d3b5
phil <uniphil@gmail.com>
2021-07-21 19:23:13 -0400

    add app_tasks task
    plus update a few things that got out of sync

--- root/home/blogger/deploy.sh
+++ root/home/blogger/deploy.sh
@@ -4,6 +4,7 @@ set -eux
 REV=$1
 export GIT_DIR=/home/blogger/commit--blog.git
 
+GIT_WORK_TREE=/var/commit--blog/ git reset --hard
 GIT_WORK_TREE=/var/commit--blog/ git -c advice.detachedHead=false checkout $REV
 GIT_WORK_TREE=/var/commit--blog/ git reset --hard
 GIT_WORK_TREE=/var/commit--blog/ git checkout -- '*'
@@ -11,6 +12,7 @@ GIT_WORK_TREE=/var/commit--blog/ git checkout -- '*'
 doas -u commit--blog /var/commit--blog/venv/bin/pip --disable-pip-version-check install -r /var/commit--blog/requirements.txt
 
 doas /usr/local/bin/supervisorctl restart gunicorn
+doas /usr/local/bin/supervisorctl restart app_tasks
 
 GIT_WORK_TREE=/var/www/htdocs/ git checkout $REV -- static
 
